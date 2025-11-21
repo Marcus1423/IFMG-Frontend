db88b433699c59be31e0af0ee278dd051e014ea2
Marcus1423 <marquinhus.vinicius10@gmail.com>
2025-11-21 16:39:45 -0300

    Atualizando a atividade

--- aulasPresensiais/aula19/atividades/atividade1/scripts.js
+++ aulasPresensiais/aula19/atividades/atividade1/scripts.js
@@ -1,6 +1,27 @@
 class Pessoa {
     #nome; #cpf; #endereco;
-    constructor(nome, cpf, endereco) {
-        
+    constructor(nome, cpf, logradoro, numero, bairro, cidade, estado, cep) {
+        this.#nome = nome;
+        this.#cpf = cpf;
+        this.#endereco = new Endereco(logradoro, numero, bairro, cidade, estado, cep);
     } 
 }
+
+class Endereco {
+    #logradoro; #numero; #bairro; 
+    #cidade; #estado; #cep;
+
+    constructor(logradoro, numero, bairro, cidade, estado, cep) {
+        this.#logradoro = logradoro;
+        this.#numero = numero;
+        this.#bairro = bairro;
+        this.#cidade = cidade;
+        this.#estado = estado;
+        this.#cep = cep;
+    }
+
+}
+
+let pessoa1 = new Pessoa ("Marcus", 123, "Rua s", "3", "Bairro s", "Cidade C", "Estado M", "1235");
+
+console.log(pessoa1);
