3df2194425c8e5e2a8569cc1c1e4fdd4bb88400c
Marcus1423 <marquinhus.vinicius10@gmail.com>
2026-01-12 16:41:30 -0300

    atualizando aula 29

---
+++ aulasPresensiais/aula29/my-vite-app/src/Lista.jsx
@@ -0,0 +1,11 @@
+function Lista (){
+
+ return (
+    <ul>
+       <li>Ferrari</li>
+       <li>Lamborghini</li> 
+    </ul>
+ )
+}
+
+export default Lista;
--- aulasPresensiais/aula29/my-vite-app/src/main.jsx
+++ aulasPresensiais/aula29/my-vite-app/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Lista from './Lista.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Lista />
   </StrictMode>,
 )
