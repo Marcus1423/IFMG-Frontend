4273b1b41ad1e62b3476b66187a9faa501384bcc
Aluno <Aluno@IBD10924P9083.ifmg.edu.br>
2026-01-12 15:34:31 -0300

    adicionando aula 29

--- aulasPresensiais/aula29/my-app/src/App.js
+++ aulasPresensiais/aula29/my-app/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
